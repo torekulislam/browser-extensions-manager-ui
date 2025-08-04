bc14585af6524ef70f9e5ad11122229c4973ca35
Torekul Islam <rihanahamad421@gmail.com>
2025-08-04 15:39:56 +0600

    footer change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="text-gray-800 text-sm w-full text-center mb-6">
+    <div className="text-gray-800 dark:text-gray-200 text-sm w-full text-center mb-6">
       Challenge by{" "}
       <a
         className="text-rose-500 hover:underline outline-rose-800"
@@ -10,8 +10,8 @@ function Footer() {
         aria-label="Forntend Mentor"
       >
         Frontend Mentor
-      </a>
-      . Coded by{" "}
+      </a>{" "}
+      Coded by{" "}
       <a
         className="text-rose-500 hover:underline outline-rose-900"
         href="https://www.linkedin.com/in/torekul-islam-72748b335/"
